d8af26a2a6cf15eb410b7c4686f2b34d389588f1
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-01-11 12:23:47 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sylvan Ash on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
